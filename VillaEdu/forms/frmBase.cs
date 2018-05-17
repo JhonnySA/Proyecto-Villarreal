c1a8f2f0cef90f7f24c5c76f3c68e5218dc1c742
Ronal <rquispe640@gmail.com>
2018-05-16 21:21:22 -0500

    prueba de comentarios

--- VillaEdu/forms/frmBase.cs
+++ VillaEdu/forms/frmBase.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace VillaEdu.forms
 {
-    public partial class frmBase : Form
+    public partial class frmBase : Form //dsdsnfdnsjddddddddddddddddddddddddddddddddddddddddddd
     {
         public frmBase()
         {
